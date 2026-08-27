4e3861689b48c5d05beaddf9ddd3846b7877c6e6
rukamori <itzmerinz.dc@gmail.com>
2026-08-27 15:51:55 +0700

    fix(HomeViewModel): prevent startup playlist resync

--- app/src/main/kotlin/moe/rukamori/archivetune/MainActivity.kt
+++ app/src/main/kotlin/moe/rukamori/archivetune/MainActivity.kt
@@ -2422,6 +2422,7 @@ class MainActivity : ComponentActivity() {
                                     navigationBuilder(
                                         navController,
                                         topAppBarScrollBehavior,
+                                        homeViewModel,
                                         { latestVersionName },
                                         disableAnimations,
                                         onClearUpdateBadge = { latestVersionName = BuildConfig.VERSION_NAME },
